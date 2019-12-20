694454832215860b8c88d094390bee7ae9a386c4
Zach Gwirtz <zachgw@gmail.com>
2019-12-20 09:49:34 -0800

    menu compnonet

---
+++ src/components/Menu/Menu.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Menu.css';
+
+export default class Menu extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            open: false
+        }
+    }
+    
+    render() {
+        return (
+            <div>
+
+            </div>
+        )
+    }
+}
