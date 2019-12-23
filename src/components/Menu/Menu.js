9fcac0473d1aea14d2be38a0fa10543f5f53386a
Zach Gwirtz <zachgw@gmail.com>
2019-12-22 19:31:09 -0800

    menu functionality

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -9,10 +9,39 @@ export default class Menu extends React.Component {
             open: false
         }
     }
+
+    openMenu() {
+        this.setState({
+            open: true
+        })
+    }
+
+    closeMenu() {
+        this.setState({
+            open: false
+        })
+    }
     
     render() {
         return (
             <div>
+                {this.state.open === false 
+                    ?   <nav className='navBar'>
+                            <div id='myDIV' className='burger burger-rotate' onClick={() => this.openMenu()}>
+                                <div className='burger-lines'></div>
+                            </div>
+                        </nav>
+                    :   <nav className='navBar'>
+                            <div className='burger burger-rotate open myNav' onClick={() => this.closeMenu()}>
+                                <div className='burger-lines'></div>
+                                <div id='myNav' className='overlay'>
+                                    <div className='overlay-content'>
+                            
+                                    </div>
+                                </div>
+                            </div>
+                        </nav>
+                }
                 <Link
                     style={{ textDecoration: 'none'}}
                     to='/'>
